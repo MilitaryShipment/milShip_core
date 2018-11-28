e3cca8f1d23c1090ad3bc94d26b4b25ef4ce1bfa
t3kj3wlz <outlawstar4761@gmail.com>
2018-11-28 15:28:54 -0600

    updated query quotes

--- models/ops/Driver.php
+++ models/ops/Driver.php
@@ -81,7 +81,7 @@ class Driver extends Record{
             ->database(self::DB)
             ->table(self::TABLE)
             ->select(self::PRIMARYKEY)
-            ->where($key,"=",$value)
+            ->where($key,"=","'" . $value . "'")
             ->get();
         while($row = mssql_fetch_assoc($results)){
             $ids[] = $row[self::PRIMARYKEY];
