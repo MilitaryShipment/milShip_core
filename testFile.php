4ac44cee2f3ddf3d4e82b2506309b81b4e8cf43b
t3kj3wlz <outlawstar4761@gmail.com>
2018-11-28 10:06:56 -0600

    updated TestFile

--- testFile.php
+++ testFile.php
@@ -2,5 +2,9 @@
 
 require_once __DIR__ . '/models/ops/Shipment.php';
 
-$s = new Shipment('FDNT0000000');
-print_r($s);
+$gbl = 'BGNC0469443';
+
+$s = new Shipment($gbl);
+print_r($s->getNotifications());
+print_r($s->getDriver());
+print_r($s->getOA());
