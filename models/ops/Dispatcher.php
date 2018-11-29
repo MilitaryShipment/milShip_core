032e483f64cf3f4bf0fc0079e283bbacbc156b79
t3kj3wlz <outlawstar4761@gmail.com>
2018-11-29 08:56:42 -0600

    updated quoting

--- models/ops/Dispatcher.php
+++ models/ops/Dispatcher.php
@@ -44,7 +44,7 @@ class Dispatcher extends Record{
             ->database(self::DB)
             ->table(self::TABLE)
             ->select(self::PRIMARYKEY)
-            ->where($key,"=",$value)
+            ->where($key,"=","'" . $value . "'")
             ->get();
         while($row = mssql_fetch_assoc($results)){
             $ids[] = $row[self::PRIMARYKEY];
