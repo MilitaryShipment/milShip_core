5e660de03f9eb136bac312ea47ce46e8974cffe8
t3kj3wlz <outlawstar4761@gmail.com>
2018-12-12 10:58:42 -0600

    updated AgentLoad

--- processes/traffic/AgentLoad.php
+++ processes/traffic/AgentLoad.php
@@ -37,8 +37,8 @@ class AgentLoad{
     );
     try{
       $this->shipment = new Shipment($gbl_dps);
-      $this->shipement->load_eta_early_time = $this->_isUntouched($this->timeInputs['load_eta_early_time']) ? $this->shipement->load_eta_early_time : $this->_buildTimeStr($this->timeInputs['load_eta_early_time']);;
-      $this->shipment->load_eta_late_time = $this->_isUntouched($this->timeInputs['load_eta_late_time']) ? $this->shipment->load_eta_late_time : $this->_buildTimeStr($this->timeInputs['load_eta_late_time']);;
+      $this->shipment->load_eta_early_time = $this->_isUntouched($this->timeInputs['load_eta_early_time']) ? $this->shipement->load_eta_early_time : $this->_buildTimeStr($this->timeInputs['load_eta_early_time']);
+      $this->shipment->load_eta_late_time = $this->_isUntouched($this->timeInputs['load_eta_late_time']) ? $this->shipment->load_eta_late_time : $this->_buildTimeStr($this->timeInputs['load_eta_late_time']);
       $this->shipment->update();
       $this->_buildMsgSubject()
             ->_buildMsgBody()
@@ -93,7 +93,7 @@ class AgentLoad{
   }
   protected function _sendMsg(){
     try{
-      Messenger::send(self::MSGTO,self::MSGFROM,self::MSGFROM,self::MSGCC,self::MSGCC,'',$this->msgSubject,$this->msgBody,'');
+      Messenger::send(self::MSGTO,self::MSGFROM,self::MSGFROM,self::MSGCC,self::MSGCC,'',$this->msgSubject,$this->msgBody,array(),true);
     }catch(\Exception $e){
       throw new \Exception($e->getMessage());
     }
