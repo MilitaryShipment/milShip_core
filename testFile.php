9e0e38f16a394b2bac51547dda1f708aae2fbeab
t3kj3wlz <outlawstar4761@gmail.com>
2018-12-18 09:44:31 -0600

    Van Ops test

--- testFile.php
+++ testFile.php
@@ -3,4 +3,17 @@
 
 require_once __DIR__ . '/processes/traffic/VanOperator.php';
 
-$v = new VanOperator();
+$gbl_dps = 'FDNT0000000';
+
+$input = new stdClass();
+$input->delivery_date_eta_early_time = "0:0";
+$input->delivery_date_eta_late_time = "0:0";
+$input->delivery_eta_date = "11/14/2018";
+$input->final_load_eta_date = "1/1/1970";
+$input->gross_weight = 0;
+$input->is_overflow = false;
+$input->necessity_item_description = "";
+$input->necessity_items_left = false;
+$input->tare_weight = 0;
+
+$v = new VanOperator($gbl_dps,$input);
