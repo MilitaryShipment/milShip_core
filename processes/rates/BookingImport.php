bf9d9b6c35c1beea6c73c392b8c4e5f931fe4903
t3kj3wlz <outlawstar4761@gmail.com>
2018-12-31 11:59:03 -0600

    bookings

--- processes/rates/BookingImport.php
+++ processes/rates/BookingImport.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . '/../../models/rates/RateFactory.php';
 require_once __DIR__ . '/../../models/rates/DpsBooking.php';
 
 
@@ -7,6 +8,8 @@ class BookingImport{
 
   const INPUTDIR = 'data/input/bookings/';
   const FILEPATT = "/([0-9]{4})\sbooking\sreport/i";
+  const LANEPATT = "/(.*)\(/";
+
   protected $inDirDir;
   protected $inputFiles = array();
 
@@ -29,9 +32,27 @@ class BookingImport{
   }
   protected function _parseInputFiles(){
     foreach($this->inputFiles as $file){
-      $csv = RateFactory::readCsv($this->inDir . $file['file']);
-      print_r($csv);
+      $rows = RateFactory::readCsv($this->inDir . $file['file']);
+      foreach($rows as $row){
+        $newBooking = new DpsBooking();
+        $newBooking->scac = $row[0];
+        $newBooking->gbl_dps = $row[3];
+        $newBooking->reg_number = $row[8];
+        $newBooking->rate = $row[9];
+        $newBooking->lane = $this->_trimLane($row[11]);
+        $newBooking->oa_state = $row[4];
+        $newBooking->da_state = $row[5];
+        $newBooking->reg_date = $row[6];
+        $newBooking->load_date = $row[2];
+        $newBooking->load_status = $row[7];
+        $newBooking->is_shortFuse = $row[10];
+        $newBooking->create();
+      }
     }
     return $this;
+  }
+  protected function _trimLane($lane){
+    preg_match(self::LANEPATT,$lane,$matches);
+    return trim(strtolower($matches[1]));
   }
 }
