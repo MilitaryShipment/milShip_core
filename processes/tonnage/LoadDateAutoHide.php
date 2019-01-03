25e039d3ac7680cae44c7e5cca6f0c5a4714d397
t3kj3wlz <outlawstar4761@gmail.com>
2019-01-03 08:18:56 -0600

    finalized

--- processes/tonnage/LoadDateAutoHide.php
+++ processes/tonnage/LoadDateAutoHide.php
@@ -4,13 +4,15 @@ require_once __DIR__ . '/../../models/ops/tonnage/TonnageList.php';
 
 class LoadDateAutoHide{
 
-  const LIMITSTR = "+2 weeks";
+  const LIMITSTR = "+3 weeks";
 
   public function __construct(){
     $list = new TonnageList();
     foreach($list->shipments as $shipment){
       if(strtotime($shipment->pickup) > strtotime(self::LIMITSTR)){
-        echo $shipment->pickup . "\n";
+        echo $shipment->gbl_dps . " -> " . $shipment->pickup . "\n";
+        $shipment->status_id = 2;
+        $shipment->update();
       }
     }
   }
