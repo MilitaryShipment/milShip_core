2134c6e5b3e30c247a340250a35f9e1a5be719ac
t3kj3wlz <outlawstar4761@gmail.com>
2019-01-08 08:10:38 -0600

    fixed cron job

--- processes/cron/AutoHideTonnage.php
+++ processes/cron/AutoHideTonnage.php
@@ -3,4 +3,4 @@
 require_once __DIR__ . '/../tonnage/LoadDateAutoHide.php';
 
 echo "Trying to autohide tonnage at: " . date('m/d/Y H:i:s') . "\n";
-$l = LoadDateAutoHide();
+$l = new LoadDateAutoHide();
