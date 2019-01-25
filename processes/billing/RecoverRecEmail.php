cda4a13e0a66bc5505c00bcf673615e4c8ef98ce
t3kj3wlz <outlawstar4761@gmail.com>
2019-01-25 11:29:30 -0600

    variables

--- processes/billing/RecoverRecEmail.php
+++ processes/billing/RecoverRecEmail.php
@@ -3,6 +3,13 @@
 require_once __DIR__ . '/../../models/dc/RecEmail.php';
 require_once __DIR__ . '/../../models/util/translateGbl.php';
 
+/*
+Old billing records use short gbl.
+Usually it's not a big deal because they don't reference like an archive
+Sometimes they need an old one and you have to update the file path to the long gbl
+tjjw1
+*/
+
 
 class RecoverRecEmail{
 
@@ -10,9 +17,9 @@ class RecoverRecEmail{
   protected $_shortGbl;
 
   public function __construct($longGbl){
-    $this->$_longGbl = $longGbl;
-    $this->$_shortGbl = $this->_getShortGbl($longGbl);
-    echo $this->$_shortGbl;
+    $this->_longGbl = $longGbl;
+    $this->_shortGbl = $this->_getShortGbl($longGbl);
+    echo $this->_shortGbl;
   }
   protected function _getShortGbl($longGbl){
     return dps2tops($longGbl);
