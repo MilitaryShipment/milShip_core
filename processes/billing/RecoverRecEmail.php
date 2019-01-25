325961ff1eec76b142d3490c9281f0474379ffe8
t3kj3wlz <outlawstar4761@gmail.com>
2019-01-25 11:35:08 -0600

    get records

--- processes/billing/RecoverRecEmail.php
+++ processes/billing/RecoverRecEmail.php
@@ -15,13 +15,30 @@ class RecoverRecEmail{
 
   protected $_longGbl;
   protected $_shortGbl;
+  protected $_records = array();
 
   public function __construct($longGbl){
     $this->_longGbl = $longGbl;
     $this->_shortGbl = $this->_getShortGbl($longGbl);
+    $this->_build();
     echo $this->_shortGbl . "\n";
+    echo count($this->_records) . "\n";
   }
   protected function _getShortGbl($longGbl){
     return dps2tops($longGbl,true);
+  }
+  protected function _build(){
+    $results = $GLOBALS['db']
+        ->suite(RecEmail::DRIVER)
+        ->driver(RecEmail::DRIVER)
+        ->database(RecEmail::DB)
+        ->table(RecEmail::TABLE)
+        ->select(RecEmail::PRIMARYKEY)
+        ->where("gbl_dps","=","'" . $this->_longGbl . "'")
+        ->get();
+    while($row = mssql_fetch_assoc($results)){
+      $this->_records[] = new RecEmail($row[RecEmail::PRIMARYKEY]);
+    }
+    return $this;
   }
 }
